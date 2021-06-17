5204a83458296b5f864e6c7761b68d34db9da9ce
Darlan <Darlan.silvadacruz@gmail.com>
2021-06-17 09:44:25 -0300

    feat: add props in habit card component

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -1,11 +1,11 @@
 import styles from "../styles/components/HabitCard.module.css";
 import IconSvg from "./IconSvg";
 
-function HabitCard() {
+function HabitCard(props) {
   return (
     <div className={styles.card}>
       <div className={styles.iconWrapper}>{/*Icon*/}</div>
-      <p>Meditation</p>
+      <p>{props.name}</p>
       <div className={styles.buttonWrapper}>
         <button className={styles.checkButton}>
           <IconSvg icon="check" height="32" width="32" color="#666666" />
