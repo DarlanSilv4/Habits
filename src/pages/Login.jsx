2b5bc40f229c44c1f1af29e1f4139905df22cd74
Darlan <darlan.silvadacruz@gmail.com>
2021-07-03 08:07:14 -0300

    feat: redirect to home page after login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,27 @@
 import styles from "../styles/pages/Login.module.css";
+
+import PageLoader from "../components/PageLoader";
+
+import { auth } from "../services/firebase";
 import { useAuth } from "../contexts/AuthContext";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Redirect } from "react-router-dom";
 
 export function Login() {
   const { signInWithGoogle } = useAuth();
+  const [user, loading, error] = useAuthState(auth);
+
+  if (error) {
+    <div>
+      <p>Error: {error.message}</p>
+    </div>;
+  }
+  if (loading) {
+    return <PageLoader />;
+  }
+  if (user) {
+    return <Redirect to={"/app"} />;
+  }
 
   return (
     <div className={styles.login}>
@@ -13,7 +32,11 @@ export function Login() {
         </div>
         <p>Focus on your routine and start building habits!</p>
       </header>
-      <button onClick={signInWithGoogle}>
+      <button
+        onClick={() => {
+          signInWithGoogle();
+        }}
+      >
         <img src="/icons/google-icon.svg" alt="google icon" />
         <p>Sign in with Google</p>
       </button>
