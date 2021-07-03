65ed41d183752c9c3653e37b218d2adfebc1a441
Darlan <darlan.silvadacruz@gmail.com>
2021-07-03 08:18:04 -0300

    feat: setup application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+import PrivateRoute from "./components/PrivateRoute";
+
 import "./styles/global.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Login} exact />
+        <PrivateRoute path="/app" component={Home} exact />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
