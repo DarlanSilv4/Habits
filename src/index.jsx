5ef7b928315ea1487b221400f62f0eed610bf058
Darlan <darlan.silvadacruz@gmail.com>
2021-07-03 08:13:20 -0300

    feat: add providers in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import AuthProvider from "./contexts/AuthContext";
+import HabitsProvider from "./contexts/HabitsContext";
+
+import "./services/firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <HabitsProvider>
+        <App />
+      </HabitsProvider>
+    </AuthProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
