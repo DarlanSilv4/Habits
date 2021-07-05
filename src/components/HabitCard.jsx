0d18da8cb0017063801b2ae17f7ac2b6e9c446ee
Darlan <darlan.silvadacruz@gmail.com>
2021-07-05 08:19:24 -0300

    feat: change habit card component
    uncheck button has been removed and now shows undo button when the habit is marked as finish

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react";
 import styles from "../styles/components/HabitCard.module.css";
 import IconSvg from "./IconSvg";
 
 function HabitCard(props) {
+  const [isConcluded, setIsConcluded] = useState(false);
+
   return (
-    <div className={styles.card}>
+    <div
+      className={
+        isConcluded ? `${styles.card} ${styles.concluded}` : styles.card
+      }
+    >
       <div className={styles.iconWrapper}>{/*Icon*/}</div>
       <p>{props.name}</p>
-      <div className={styles.buttonWrapper}>
-        <button className={styles.checkButton}>
+      {isConcluded ? (
+        <button title="Mark as unfinished" className={styles.undoButton}>
+          <IconSvg icon="undo" height="32" width="32" color="#FFFFFF" />
+        </button>
+      ) : (
+        <button title="Mark as finish" className={styles.checkButton}>
           <IconSvg icon="check" height="32" width="32" color="#666666" />
         </button>
-        <button className={styles.cancelButton}>
-          <IconSvg icon="cancel" height="32" width="32" color="#666666" />
-        </button>
-      </div>
+      )}
     </div>
   );
 }
