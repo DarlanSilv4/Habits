ac865a2a4fd3b91904fd8c35f4432dd22bf1160a
Darlan <darlan.silvadacruz@gmail.com>
2021-07-19 12:14:11 -0300

    feat: create calendar component

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/components/Calendar.module.css";
+
+function Calendar({ year, month, habitsConcludedInThisMonth }) {
+  const [days, setDays] = useState([]);
+
+  const isToday = (day) => {
+    const date = new Date();
+    const today = date.getDate();
+    const currentMonth = date.getMonth();
+    const currentYear = date.getFullYear();
+
+    if (day === today && month === currentMonth && year === currentYear)
+      return styles.today;
+
+    return "";
+  };
+
+  const thereHabitsConcludedToday = (day) => {
+    const stringDay = day.toString();
+
+    if (habitsConcludedInThisMonth.length > 0) {
+      if (habitsConcludedInThisMonth.includes(stringDay))
+        return styles.completedDay;
+    }
+    return "";
+  };
+
+  const setupDays = () => {
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    const firstWeekday = firstDay.getDay();
+
+    const updatedDays = [];
+
+    for (let i = 1; i <= firstWeekday; i++) {
+      updatedDays.push(
+        <p key={i * -1} className={styles.day}>
+          {" "}
+        </p>
+      );
+    }
+
+    for (let day = 1; day <= lastDay.getDate(); day++) {
+      updatedDays.push(
+        <p
+          key={day}
+          className={`${styles.day} ${isToday(day)} ${thereHabitsConcludedToday(
+            day
+          )}`}
+        >
+          {day}
+        </p>
+      );
+    }
+
+    setDays(updatedDays);
+  };
+
+  useEffect(() => {
+    setupDays();
+  }, [habitsConcludedInThisMonth]);
+
+  return (
+    <div className={styles.calendar}>
+      <div className={styles.weekdays}>
+        <p>Sun</p>
+        <p>Mon</p>
+        <p>Tue</p>
+        <p>Wed</p>
+        <p>Thu</p>
+        <p>Fri</p>
+        <p>Sat</p>
+      </div>
+
+      <div className={styles.dayContainer}>{days}</div>
+    </div>
+  );
+}
+
+export default Calendar;
