9bcfca2cb689ee88bcddd32ccee7e20c392a611f
Darlan <darlan.silvadacruz@gmail.com>
2021-08-06 09:54:15 -0300

    refactor(home.jsx): change the use of habits context to modals context
    use isNewHabitModalOpen from modals context instead of habits context
    
    BREAKING CHANGE: habits context is no longer used in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@ import CreateNewHabit from "../components/CreateNewHabit";
 import TopNavigationBar from "../components/TopNavigationBar";
 import HabitList from "../components/HabitList";
 import StreakMap from "../components/StreakMap";
-import { useHabits } from "../contexts/HabitsContext";
 import { useAuth } from "../contexts/AuthContext";
+import { useModals } from "../contexts/ModalsContext";
 
 function Home() {
-  const { isNewHabitModalOpen } = useHabits();
+  const { isNewHabitModalOpen } = useModals();
   const { user } = useAuth();
 
   return (
