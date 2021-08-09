988f4c0572145a89d3bf1031f3aad1b086211863
Darlan <darlan.silvadacruz@gmail.com>
2021-08-09 09:19:36 -0300

    feat(home.jsx): add profile menu component into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,18 @@ import CreateNewHabit from "../components/CreateNewHabit";
 import TopNavigationBar from "../components/TopNavigationBar";
 import HabitList from "../components/HabitList";
 import StreakMap from "../components/StreakMap";
+import ProfileMenu from "../components/ProfileMenu";
 import { useAuth } from "../contexts/AuthContext";
 import { useModals } from "../contexts/ModalsContext";
 
 function Home() {
-  const { isNewHabitModalOpen } = useModals();
+  const { isNewHabitModalOpen, isProfileMenuOpen } = useModals();
   const { user } = useAuth();
 
   return (
     <>
       {isNewHabitModalOpen ? <CreateNewHabit /> : <></>}
+      {isProfileMenuOpen ? <ProfileMenu /> : <></>}
       <TopNavigationBar user={user} />
       <main className="container">
         <HabitList />
