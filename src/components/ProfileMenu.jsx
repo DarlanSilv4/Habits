425af2af1d4c771ff09af42063cb8ee93a09bb3b
Darlan <darlan.silvadacruz@gmail.com>
2021-08-09 09:17:17 -0300

    feat(profile menu component): add overlay background

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -5,11 +5,13 @@ function ProfileMenu() {
   const { user, signOut } = useAuth();
 
   return (
-    <div className={styles.profileMenuWrapper}>
-      <img src={user.avatar} alt="profile" className={styles.picture} />
-      <p>{user.name}</p>
-      <div className={styles.line}></div>
-      <button onClick={signOut}>Sign out</button>
+    <div className={styles.overlay}>
+      <div className={styles.profileMenuWrapper}>
+        <img src={user.avatar} alt="profile" className={styles.picture} />
+        <p>{user.name}</p>
+        <div className={styles.line}></div>
+        <button onClick={signOut}>Sign out</button>
+      </div>
     </div>
   );
 }
