7229b20c7d632d0848759da64d51913ff000fc47
Darlan <darlan.silvadacruz@gmail.com>
2021-09-03 10:12:01 -0300

    feat(habit card component): add edit button and delete button
    add edit button and delete button that only appear in '/habit' route
    
    BREAKING CHANGE: cards button change depending on the route

--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/components/HabitCard.module.css";
 import IconSvg from "./IconSvg";
 import cx from "classnames";
+import { Route } from "react-router-dom";
 
 function HabitCard({
   name,
@@ -19,29 +20,45 @@ function HabitCard({
     <div className={habitCardStyle}>
       <div className={styles.iconWrapper}>{/*Icon*/}</div>
       <p>{name}</p>
-      {isConcluded ? (
-        <button
-          title="Mark as unfinished"
-          className={styles.undoButton}
-          onClick={() => {
-            handleStreak.undoUpdateStreak();
-            handleCompleteHabit();
-          }}
-        >
-          <IconSvg icon="undo" height="32" width="32" color="#FFFFFF" />
-        </button>
-      ) : (
-        <button
-          title="Mark as finish"
-          className={styles.checkButton}
-          onClick={() => {
-            handleStreak.updateStreak();
-            handleCompleteHabit();
-          }}
-        >
-          <IconSvg icon="check" height="32" width="32" color="#666666" />
-        </button>
-      )}
+      <Route path="/app">
+        {isConcluded ? (
+          <button
+            title="Mark as unfinished"
+            className={styles.undoButton}
+            onClick={() => {
+              handleStreak.undoUpdateStreak();
+              handleCompleteHabit();
+            }}
+          >
+            <IconSvg icon="undo" height="32" width="32" color="#FFFFFF" />
+          </button>
+        ) : (
+          <button
+            title="Mark as finish"
+            className={styles.checkButton}
+            onClick={() => {
+              handleStreak.updateStreak();
+              handleCompleteHabit();
+            }}
+          >
+            <IconSvg icon="check" height="32" width="32" color="#666666" />
+          </button>
+        )}
+      </Route>
+      <Route path="/habits">
+        <div className={styles.buttonGroup}>
+          <button title="Edit" className={styles.editButton} onClick={() => {}}>
+            <IconSvg icon="edit" height="28" width="28" color="#666666" />
+          </button>
+          <button
+            title="Delete"
+            className={styles.deleteButton}
+            onClick={() => {}}
+          >
+            <IconSvg icon="delete" height="28" width="28" color="#666666" />
+          </button>
+        </div>
+      </Route>
     </div>
   );
 }
